9b4bc1aed7b85b61bb1746c81a259993a4129449
jneira <atreyu.bbb@gmail.com>
2017-05-12 00:44:17 +0200

    some java bindings

--- src/Network/Wai/Servlet/Handler/Jetty.hs
+++ src/Network/Wai/Servlet/Handler/Jetty.hs
@@ -1,20 +1,56 @@
-{-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
-             FlexibleContexts,TypeOperators #-}
+{-# LANGUAGE MagicHash, TypeFamilies, DataKinds,
+             FlexibleContexts, TypeOperators,
+             MultiParamTypeClasses #-}
 module Network.Wai.Servlet.Handler.Jetty where
-import Network.Wai               
-import Network.Wai.Servlet.Request
-import Network.Wai.Servlet.Response
-import Java
+import qualified Network.Wai          as Wai     
+import           Network.Wai.Servlet  as JWai
+import           Java
 
-data {-# CLASS org.eclipse.jetty.server.handler.AbstractHandler -}
-   AbstractHandler = AbstractHandler (Object# AbstractHandler)
-data {-# CLASS org.eclipse.jetty.server.Request -}
-   Request = Request (Object# Request) 
+data {-# CLASS "org.eclipse.jetty.server.handler.AbstractHandler" #-}
+  AbstractHandler = AbstractHandler (Object# AbstractHandler)
+  deriving Class
+
+data {-# CLASS "org.eclipse.jetty.server.Request" #-}
+  Request = Request (Object# Request) 
+  deriving Class
 
 foreign import java unsafe "@wrapper @abstract handle"
-   abstractHandler :: Request -> ServletRequest -> ServletResponse ->
-                      AbstractHandler 
+  abstractHandler :: (Request -> ServletRequest -> ServletResponse ->
+                      Java AbstractHandler ()) -> AbstractHandler 
 
---makeAbstract
+data {-# CLASS "org.eclipse.jetty.server.ServerConnector" #-}
+  ServerConnector = ServerConnector (Object# ServerConnector)
+  deriving Class
+
+data {-# CLASS "org.eclipse.jetty.server.Server" #-}
+  Server = Server (Object# Server)
+  deriving Class
+
+data {-# CLASS "org.eclipse.jetty.server.ConnectionFactory" #-}
+  ConnectionFactory = ConnectionFactory (Object# ConnectionFactory)
+  deriving Class
+
+data {-# CLASS "org.eclipse.jetty.server.ConnectionFactory[]" #-}
+  ConnectionFactoryArray = ConnectionFactoryArray
+     (Object# ConnectionFactoryArray) deriving Class
+
+instance JArray ConnectionFactory ConnectionFactoryArray
+
+foreign import java unsafe "@new" serverConnector ::
+  Server -> ConnectionFactoryArray -> ServerConnector
+
+data {-# CLASS "org.eclipse.jetty.server.HttpConfiguration" #-}
+  HttpConfiguration = HttpConfiguration (Object# HttpConfiguration)
+  deriving Class
+
+foreign import java unsafe "@new" httpConfiguration :: HttpConfiguration
+
+data Options = Options 
+
+httpConfig :: Options -> HttpConfiguration
+httpConfig opts = undefined
+
+httpConnector :: Server -> Options -> ServerConnector
+httpConnector serv opts = undefined
 
 
